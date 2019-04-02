cce941a577b3134b351e5ea0d80147b3b2b6c496
ian douglas <ian.douglas@iandouglas.com>
2019-04-02 12:49:38 -0600

    all controllers and templates to cover all nav testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,50 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :current_admin?, :current_merchant?, :current_reguser?
+  before_action :build_cart
+
+  def build_cart
+    @cart ||= Cart.new(session[:cart])
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_merchant?
+    current_user && current_user.merchant?
+  end
+
+  def current_reguser?
+    current_user && current_user.default?
+  end
+
+  def visitor_or_user
+    render file: 'public/404', status: 404 unless current_user.nil? || current_reguser?
+  end
+
+  # def user_or_admin
+  #   render file: 'public/404', status: 404 unless current_user && (current_reguser? || current_admin?)
+  # end
+
+  def require_reguser
+    render file: 'public/404', status: 404 unless current_reguser?
+  end
+
+  def require_merchant
+    render file: 'public/404', status: 404 unless current_merchant?
+  end
+
+  # def merchant_or_admin
+  #   render file: 'public/404', status: 404 unless current_merchant? || current_admin?
+  # end
+
+  def require_admin
+    render file: 'public/404', status: 404 unless current_admin?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get '/login', to: 'sessions#new', as: :login
+  get '/logout', to: 'sessions#destroy', as: :logout
+  get '/register', to: 'users#new', as: :registration
+
+  get '/cart', to: 'cart#show'
+
+  resources :items, only: [:index]
+  resources :merchants, only: [:index]
+
+  get '/profile', to: 'users#show', as: :profile
+  namespace :profile do
+    resources :orders, only: [:index,]
+  end
+
+  namespace :dashboard do
+    get '/', to: 'dashboard#index'
+    resources :items, only: [:index]
+  end
+
+  namespace :admin do
+    get '/dashboard', to: 'dashboard#index'
+    resources :users, only: [:index, :show]
+    resources :merchants, only: [:show]
+  end
 end

03975d8a10f268f8daf8a2451b39b90d7eddc244
Brian Zanti <brianzanti@gmail.com>
2019-04-02 15:10:18 -0600

    adds tests for roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,50 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
   end
+
+  describe 'roles' do
+    it 'can be created as a default user' do
+      user = User.create(
+        email: "email",
+        password: "password",
+        name: "name",
+        address: "address",
+        city: "city",
+        state: "state",
+        zip: "zip"
+      )
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
+    end
+
+    it 'can be created as a merchant' do
+      user = User.create(
+        email: "email",
+        password: "password",
+        name: "name",
+        address: "address",
+        city: "city",
+        state: "state",
+        zip: "zip",
+        role: 1
+      )
+      expect(user.role).to eq('merchant')
+      expect(user.merchant?).to be_truthy
+    end
+
+    it 'can be created as an admin' do
+      user = User.create(
+        email: "email",
+        password: "password",
+        name: "name",
+        address: "address",
+        city: "city",
+        state: "state",
+        zip: "zip",
+        role: 2
+      )
+      expect(user.role).to eq('admin')
+      expect(user.admin?).to be_truthy
+    end
+  end
 end

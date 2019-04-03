7bd6abb36f45fb74c430f325d5f659689c23e33a
ian douglas <ian.douglas@iandouglas.com>
2019-04-03 17:20:01 -0600

    implements popular item stats, closes #63

---
+++ db/migrate/20190403231217_create_order_items.rb
@@ -0,0 +1,14 @@
+class CreateOrderItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :order_items do |t|
+      t.references :order, foreign_key: true
+      t.references :item, foreign_key: true
+
+      t.integer :quantity, default: 0
+      t.decimal :price, default: 0
+      t.boolean :fulfilled, default: false
+
+      t.timestamps
+    end
+  end
+end

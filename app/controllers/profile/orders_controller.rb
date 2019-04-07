f00c8eeb1196c1cb8c916428c59871bafabc930d
ian douglas <ian.douglas@iandouglas.com>
2019-04-07 09:22:22 -0600

    user can cancel an order, closes #45

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -9,4 +9,24 @@ class Profile::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def destroy
+    @order = Order.find(params[:id])
+    if @order.user == current_user
+      @order.order_items.where(fulfilled: true).each do |oi|
+        item = Item.find(oi.item_id)
+        item.inventory += oi.quantity
+        item.save
+        oi.fulfilled = false
+        oi.save
+      end
+
+      @order.status = :cancelled
+      @order.save
+
+      redirect_to profile_orders_path
+    else
+      render file: 'public/404', status: 404
+    end
+  end
 end

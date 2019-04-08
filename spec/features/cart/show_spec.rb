ecc58f623cff77d6a37ab2df94fa4dfaf1cc7180
Brian Zanti <brianzanti@gmail.com>
2019-04-08 14:11:08 -0600

    checks for case when cart is empty

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Cart show page" do
       click_on "Add to Cart"
 
       visit cart_path
-
+      
       expect(page).to have_content("Total: $#{@item_1.price + @item_2.price + @item_3.price * 2}")
 
       expect(page).to have_link("Empty Cart")
@@ -40,7 +40,7 @@ RSpec.describe "Cart show page" do
         expect(page).to have_content(@item_1.user.name)
         expect(page).to have_content(@item_1.price)
         expect(page).to have_content("quantity: 1")
-        expect(page).to have_content("subtotal: #{@item_1.price}")
+        expect(page).to have_content("subtotal: $#{@item_1.price}")
       end
 
       within("#item-#{@item_2.id}") do
@@ -49,7 +49,7 @@ RSpec.describe "Cart show page" do
         expect(page).to have_content(@item_2.user.name)
         expect(page).to have_content(@item_2.price)
         expect(page).to have_content("quantity: 1")
-        expect(page).to have_content("subtotal: #{@item_2.price}")
+        expect(page).to have_content("subtotal: $#{@item_2.price}")
       end
 
       within("#item-#{@item_3.id}") do
@@ -58,8 +58,25 @@ RSpec.describe "Cart show page" do
         expect(page).to have_content(@item_3.user.name)
         expect(page).to have_content(@item_3.price)
         expect(page).to have_content("quantity: 2")
-        expect(page).to have_content("subtotal: #{@item_3.price * 2}")
+        expect(page).to have_content("subtotal: $#{@item_3.price * 2}")
       end
+    end
+  end
+
+  context "a regular user or visitor sees an empty cart message" do
+    scenario "as a regular user" do
+      user = create(:user)
+      login_as(user)
+    end
+
+    scenario "as a visitor" do
+    end
+
+    after :each do
+      visit cart_path
+
+      expect(page).to have_content("Your Cart is empty.")
+      expect(page).to_not have_link("Empty Cart")
     end
   end
 end

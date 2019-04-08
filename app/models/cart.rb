dcb3dbbae42a6e393d446e25eeda6520a8ead7cb
Brian Zanti <brianzanti@gmail.com>
2019-04-08 12:03:18 -0600

    implements Cart PORO

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,9 +3,18 @@ class Cart
 
   def initialize(initial_contents)
     @contents = initial_contents || Hash.new(0)
+    @contents.default = 0
   end
 
   def total_item_count
     @contents.values.sum
+  end
+
+  def add_item(item_id)
+    @contents[item_id.to_s] = count_of(item_id) + 1
+  end
+
+  def count_of(item_id)
+    @contents[item_id.to_s]
   end
 end

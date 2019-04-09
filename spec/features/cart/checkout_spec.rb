35ec7495a810fd8d316ed90b40dc30e29bf887ac
Brian Zanti <brianzanti@gmail.com>
2019-04-09 11:15:28 -0600

    implements checking out

---
+++ spec/features/cart/checkout_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Checking out" do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @item_1 = create(:item, user: @merchant_1, inventory: 3)
+    @item_2 = create(:item, user: @merchant_2)
+    @item_3 = create(:item, user: @merchant_2)
+
+    visit item_path(@item_1)
+    click_on "Add to Cart"
+    visit item_path(@item_2)
+    click_on "Add to Cart"
+    visit item_path(@item_3)
+    click_on "Add to Cart"
+    visit item_path(@item_3)
+    click_on "Add to Cart"
+  end
+
+  context "as a logged in regular user" do
+    it "should create a new order" do
+      user = create(:user)
+      login_as(user)
+      visit cart_path
+      click_button "Check Out"
+
+      expect(current_path).to eq(profile_orders_path)
+      new_order = Order.last
+      expect(page).to have_content("Your order has been created!")
+      expect(page).to have_content("Cart: 0")
+      within("#order-#{new_order.id}") do
+        expect(page).to have_link("Order ID #{new_order.id}")
+        expect(page).to have_content("Status: pending")
+      end
+    end
+  end
+end

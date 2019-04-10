fcfdc3229b13fb0d928a6d7924c3b51a51c4c6fc
Brian Zanti <brianzanti@gmail.com>
2019-04-10 12:56:54 -0600

    adds button for admin to upgrade user to merchant

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,15 @@
 class Admin::UsersController < Admin::BaseController
   def index
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def upgrade
+    user = User.find(params[:id])
+    user.update(role: :merchant)
+    flash[:success] = "#{user.name} is now a merchant."
+    redirect_to admin_merchant_path(user)
+  end
 end
